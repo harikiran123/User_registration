ec85fdaf9f049e17495aac7f3b18c7bc4e1288db
harikiran <neeliharikiran@gmail.com>
2025-02-10 12:07:02 +0530

    [harikiran] Add: python script to check fro the first name

--- user.py
+++ user.py
@@ -1,1 +1,9 @@
-print("hello world")
+first_name =str(input("enter the first name : "))
+if len(first_name) > 3:
+    if first_name[0].isupper():
+        print(f"enter user name is true: {first_name}")
+    else:
+        print("enter a valid user name")
+else:
+    print("enter a valid user name")
+    
