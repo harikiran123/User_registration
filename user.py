a890b77ad29d2688cae52d9bd68c8344e0068a53
harikiran <neeliharikiran@gmail.com>
2025-02-10 15:02:12 +0530

    [harikiran] Add: python script for both first and last name

--- user.py
+++ user.py
@@ -1,9 +1,10 @@
 first_name =str(input("enter the first name : "))
-if len(first_name) > 3:
-    if first_name[0].isupper():
-        print(f"enter user name is true: {first_name}")
+last_name=str(input("enter the last name : "))
+if len(first_name) > 3 and len(last_name) > 3:
+    if first_name[0].isupper() and last_name[0].isupper():
+        print(f"entered  first name '{first_name}' is true")
+        print(f"entered last name '{last_name}' is true")
     else:
         print("enter a valid user name")
 else:
     print("enter a valid user name")
-    
