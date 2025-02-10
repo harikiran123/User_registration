5bbd5dc5bc289c2a77e4e863715c679e84fc7609
harikiran <neeliharikiran@gmail.com>
2025-02-10 16:15:30 +0530

    [harikiran] Add: python script for mobile number

--- user.py
+++ user.py
@@ -34,14 +34,32 @@ def get_email():
         else:
             print("error: enter the cotterct email formate")
 
-
-
+def get_country_code():
+    while True:
+        country_code=input("enter the country code : ")
+        if country_code.isdigit():
+            return country_code
+        else:
+            print("error:country code should be in interger value ")
+def get_mobile_number():
+    while True:
+        mobile_number=input("enter the moblie number")
+        if len(mobile_number) == 10:
+            if mobile_number.isdigit():
+                return mobile_number
+            else:
+                print("error:mobile number must be in integer")
+        else:
+            print("error: the mobile number should be in 10 digits")
+            
 
 
 
 first_name = get_first_name()
 last_name = get_last_name()
 e_mail=get_email()
+country_code=get_country_code()
+mobile_number=get_mobile_number()
 
 
 
@@ -50,3 +68,4 @@ e_mail=get_email()
 print(f"Entered first name '{first_name}' is valid.")
 print(f"Entered last name '{last_name} is valid")
 print(f"enteted email {e_mail} is valid")
+print(f"entered mobile number '{country_code} {mobile_number}'is valid")
