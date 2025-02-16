5c559ca71bef47a8d523c08fcaa5a4487600f5da
harikiran <neeliharikiran@gmail.com>
2025-02-16 15:01:04 +0530

    [harikiran] Add:python updated script for first name

--- user.py
+++ user.py
@@ -1,9 +1,22 @@
-first_name =str(input("enter the first name : "))
-if len(first_name) > 3:
-    if first_name[0].isupper():
-        print(f"enter user name is true: {first_name}")
-    else:
-        print("enter a valid user name")
-else:
-    print("enter a valid user name")
+import re
+
+
+def check_first_name():
+    while True:
+        first_name=input("enter the first name : ")
+        name_pattern=r"^[A-Z][a-z]{2,}$"
+        if re.match(name_pattern,first_name):
+            return first_name
+        else:
+            print("error: enter the correct value")
+   
+
+
+if __name__=='__main__':
+    first_name = check_first_name()
+    print(f"Valid First Name:{first_name}")
+
+    
+    
+
     
