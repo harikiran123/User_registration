20454fad12a24ca83d4d5b3a263d2b79d5357954
harikiran <neeliharikiran@gmail.com>
2025-02-16 16:37:38 +0530

    [harikiran] Add:python updated script for vlidation of special characters in password

--- user.py
+++ user.py
@@ -1,89 +1,68 @@
 import re
+ 
 
-def get_first_name():
+def check_first_name():
     while True:
-        f_name = input("Enter the first name: ")
-        
-        if len(f_name) > 2:  
-            if f_name[0].isupper():  
-                return f_name  
-            else:
-                print("Error: First name must start with an uppercase letter.")
+        first_name=input("enter the first name : ")
+        name_pattern=r"^[A-Z][a-z]{2,}$"
+        if re.match(name_pattern,first_name):
+            return first_name
         else:
-            print("Error: First name must contain at least 3 letters.")
+            print("error: enter the correct value")
 
-
-def get_last_name():
+def check_last_name():
     while True:
-        l_name= input("enter the last name : ")
-        if len(l_name) > 2:
-            if l_name[0].isupper():
-                return l_name
-            else:
-                print("error: last name must start with an uppercase letter ")
+        last_name=input("enter the last name : ")
+        name_pattern=r"^[A-Z][a-z]{2,}$"
+        if re.match(name_pattern,last_name):
+            return last_name
         else:
-            print("error: last name must contain at least 3 letters")
+            print("error: enter the correct value")
 
-def get_email():
-
+def check_email():
     while True:
-        e_mail=input("enter the email: ")
-        mail_patteren = r"^[a-zA-Z0-9/.+-_]+@gmail\.com$"
-        if re.match(mail_patteren,e_mail):
+        e_mail=input("enter the mail : ")
+        mail_pattern=r"^[a-zA-Z0-9.+_-]+@[a-z0-9-]+(\.[a-zA-Z]{2,})+$"
+        if re.match(mail_pattern,e_mail):
             return e_mail
         else:
-            print("error: enter the cotterct email formate")
+            print("error: enter the correct email")
 
-def get_country_code():
+def check_mobile_number():
     while True:
-        country_code=input("enter the country code : ")
-        if country_code.isdigit():
-            return country_code
+        mobile_number=input("enter the mobile number : ")
+        mobile_pattern=r"^\+?[0-9]{1,3}[ ]?[0-9]{10}$"
+        if re.match(mobile_pattern,mobile_number):
+            return mobile_number
         else:
-            print("error:country code should be in interger value ")
-def get_mobile_number():
+            print("error: enter the correct country code and mobile number")
+
+def check_password_eight():
     while True:
-        mobile_number=input("enter the moblie number")
-        if len(mobile_number) == 10:
-            if mobile_number.isdigit():
-                return mobile_number
-            else:
-                print("error:mobile number must be in integer")
+        password=input("enter the password : ")
+        password_pattern = r"^(?=.*[A-Z])(?=.*[-!@#$%^&*()_+=/]).{8,}$"
+        if re.match(password_pattern,password):
+            return password
         else:
-            print("error: the mobile number should be in 10 digits")
-            
-def get_password():
+            print("error: enter the correct password")
+       
+def main():
+    first_name = check_first_name()
+    last_name = check_last_name()
+    e_mail=check_email()
+    mobile_number = check_mobile_number()
+    password=check_password_eight()
+    print(f"Valid First Name :{first_name}")
+    print(f"valid last name :{last_name}")
+    print(f"valid e_mail :{e_mail}")
+    print(f"valid moblile number :{mobile_number}")
+    print(f"valid password :{password}")
+
+
+if __name__=='__main__':
+    main()
+   
+
     
-    while True:
-        password= input("enter the password: ")
-        if len(password) > 7:
-            if re.search(r"[A-Z]",password):
-                if re.search(r"[0-9]",password):
-                    return password
-                else:
-                    print("error: password must contain atleast one numeric value")
-            
-                
-            else:
-                print("error: passwor must contain atleast one upper case")
-        else:
-            print("error:password should caontain min 8 characters")
-            
-        
-
-
-first_name = get_first_name()
-last_name = get_last_name()
-e_mail=get_email()
-country_code=get_country_code()
-mobile_number=get_mobile_number()
-password=get_password()
-
-
-
-
-print(f"Entered first name '{first_name}' is valid.")
-print(f"Entered last name '{last_name} is valid")
-print(f"enteted email {e_mail} is valid")
-print(f"entered mobile number '{country_code} {mobile_number}'is valid")
-print(f"entered password {password} is valid")
+    
+    
