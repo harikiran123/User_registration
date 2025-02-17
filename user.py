9f182920960fc6fcdcae4035ce3701064aff8bfa
harikiran <neeliharikiran@gmail.com>
2025-02-17 11:49:33 +0530

    [harikiran] Add: python script password special char checker  with dot string

--- user.py
+++ user.py
@@ -2,6 +2,13 @@ import re
  
 
 def check_first_name():
+
+    '''
+    Description: This function will check if the given first_name is in the correct format or not.
+    Parameters: None
+    Return: first_name
+    '''
+    
     while True:
         first_name=input("enter the first name : ")
         name_pattern=r"^[A-Z][a-z]{2,}$"
@@ -11,6 +18,13 @@ def check_first_name():
             print("error: enter the correct value")
 
 def check_last_name():
+
+    '''
+    Description: This function will check if the given last_name is in the correct format or not.
+    Parameters: None
+    Return: last_name
+    '''
+
     while True:
         last_name=input("enter the last name : ")
         name_pattern=r"^[A-Z][a-z]{2,}$"
@@ -20,6 +34,13 @@ def check_last_name():
             print("error: enter the correct value")
 
 def check_email():
+
+    '''
+    Description: This function validates whether the given email address is in the correct format or not.
+    Parameters: None
+    Return: e_mail 
+    '''
+
     while True:
         e_mail=input("enter the mail : ")
         mail_pattern=r"^[a-zA-Z0-9.+_-]+@[a-z0-9-]+(\.[a-zA-Z]{2,})+$"
@@ -29,6 +50,14 @@ def check_email():
             print("error: enter the correct email")
 
 def check_mobile_number():
+
+    '''
+    Description: This function validates whether the given mobile number is in the correct format, 
+    including an optional country code.
+    Parameters: None
+    Return: mobile_number 
+    '''
+
     while True:
         mobile_number=input("enter the mobile number : ")
         mobile_pattern=r"^\+?[0-9]{1,3}[ ]?[0-9]{10}$"
@@ -38,6 +67,17 @@ def check_mobile_number():
             print("error: enter the correct country code and mobile number")
 
 def check_password_eight():
+
+    '''
+    Description: This function validates whether the given password meets the required criteria:
+    - At least 8 characters long
+    - Contains at least one uppercase letter
+    - Contains at least one digit
+    - Contains at least one special character
+    Parameters: None
+    Return: password 
+    '''
+
     while True:
         password=input("enter the password : ")
         password_pattern = r"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=]).{8,}$"
@@ -46,7 +86,14 @@ def check_password_eight():
         else:
             print("error: enter the correct password")
        
-def main():
+def main(): 
+
+    '''
+    Description: This function calls check_first_name and check_last_name functions and prints valid names.
+    Parameters: None
+    Return: None
+    '''
+
     first_name = check_first_name()
     last_name = check_last_name()
     e_mail=check_email()
