d5dfb150aa6cddd59d35cc1a01691d394b8d2364
harikiran <neeliharikiran@gmail.com>
2025-02-17 11:06:32 +0530

    [harikiran] Add:python script for last name with dot string

--- user.py
+++ user.py
@@ -2,6 +2,13 @@ import re
 
 
 def check_first_name():
+
+    '''
+    Description: This function will check if the given first_name is in the correct format or not.
+    Parameters: None
+    Return: first_name
+    '''
+
     while True:
         first_name=input("enter the first name : ")
         name_pattern=r"^[A-Z][a-z]{2,}$"
@@ -11,6 +18,13 @@ def check_first_name():
             print("error: enter the correct value")
 
 def check_last_name():
+
+    '''
+    Description: This function will check if the given last_name is in the correct format or not.
+    Parameters: None
+    Return: last_name
+    '''
+
     while True:
         last_name=input("enter the last name : ")
         name_pattern=r"^[A-Z][a-z]{2,}$"
@@ -20,6 +34,13 @@ def check_last_name():
             print("error: enter the correct value")
         
 def main():
+
+    '''
+    Description: This function calls check_first_name and check_last_name functions and prints valid names.
+    Parameters: None
+    Return: None
+    '''
+    
     first_name = check_first_name()
     last_name = check_last_name()
     print(f"Valid First Name :{first_name}")
