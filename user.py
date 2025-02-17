f60bba9e72849b613f46239a99ca56c35595a2d5
harikiran <neeliharikiran@gmail.com>
2025-02-17 11:54:42 +0530

    [harikiran] Add: python script password checker  with dot string

--- user.py
+++ user.py
@@ -1,7 +1,27 @@
 import re
-e_mail=input("enter the email : ")
-e_mail_pattern=r"^[a-zA-Z0-9.+_-]+@[a-z0-9-]+(\.[a-zA-Z]{2,})+$"
-if re.fullmatch(e_mail_pattern,e_mail):
-    print("valid email")
-else:
-    print("invalid")
+
+def check_email():
+
+    '''
+    Description: This function validates whether the given email address is in the correct format.
+    - Contains alphanumeric characters, dots, underscores, plus, or hyphens before '@'
+    - Contains a domain name with at least one dot
+     - Ends with a valid top-level domain (TLD)
+    Parameters: None
+    Return: None 
+    '''
+
+    e_mail = input("Enter the email: ")
+    e_mail_pattern = r"^[a-zA-Z0-9.+_-]+@[a-z0-9-]+(\.[a-zA-Z]{2,})+$"
+    
+    if re.fullmatch(e_mail_pattern, e_mail):
+        print("Valid email")
+    else:
+        print("Invalid email")
+
+def main():
+    check_email()
+
+
+if __name__ == '__main__':
+    main()
